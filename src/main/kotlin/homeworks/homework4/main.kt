78cbfef029df8aaabea652d9b81c1b88ff195105
ythosa <vasus714@yandex.ru>
2022-04-21 08:42:41 +0300

    feat: dutch flag partition

--- src/main/kotlin/homeworks/homework4/main.kt
+++ src/main/kotlin/homeworks/homework4/main.kt
@@ -4,6 +4,7 @@ import homeworks.homework4.gen.RandomListGenerator
 import homeworks.homework4.qsort.QSortCoroutines
 import homeworks.homework4.qsort.QSortSequential
 import homeworks.homework4.qsort.QSortThreadPool
+import homeworks.homework4.qsort.partitions.DutchFlagPartition
 import homeworks.homework4.qsort.partitions.HoarePartition
 import homeworks.homework4.qsort.partitions.LomutoPartition
 import java.util.concurrent.ForkJoinPool
@@ -13,11 +14,11 @@ fun main() {
     val l = RandomListGenerator.build {
         minValue = 0
         maxValue = 1000
-        elementsCount = 1000
+        elementsCount = 1000000
     }.generate().toMutableList()
 
 //    val qSort = QSortThreadPool(LomutoPartition(), ForkJoinPool(10))
-    val qSort = QSortCoroutines<Int>(HoarePartition())
+    val qSort = QSortCoroutines<Int>(DutchFlagPartition())
 //    val qSort = QSortSequential(LomutoPartition())
     qSort.sorted(l)
     println(l == l.sorted())
