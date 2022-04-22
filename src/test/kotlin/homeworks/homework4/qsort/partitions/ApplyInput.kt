941da50366a6bf496799536879dd6b696557e357
ythosa <vasus714@yandex.ru>
2022-04-22 16:52:53 +0300

    refactor: colon spaces

--- src/test/kotlin/homeworks/homework4/qsort/partitions/ApplyInput.kt
+++ src/test/kotlin/homeworks/homework4/qsort/partitions/ApplyInput.kt
@@ -1,6 +1,6 @@
 package homeworks.homework4.qsort.partitions
 
-data class ApplyInput<T: Comparable<T>>(
+data class ApplyInput<T : Comparable<T>>(
     val list: MutableList<T>,
     val lowIndex: Int,
     val highIndex: Int
