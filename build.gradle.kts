d3c10d768c785b5d746543b99936d0f333d706c7
ythosa <vasus714@yandex.ru>
2022-04-24 13:53:02 +0300

    feat: javafx dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,25 @@
+import org.gradle.nativeplatform.platform.internal.DefaultNativePlatform
+
+fun getJavaFXPlatform(): String {
+    val currentOS = DefaultNativePlatform.getCurrentOperatingSystem()
+    if (currentOS.isWindows) {
+        return "win"
+    } else if (currentOS.isLinux) {
+        return "linux"
+    } else if (currentOS.isMacOsX) {
+        return "mac"
+    }
+    throw IllegalStateException("Unexpected OS: ${currentOS.name}")
+}
+
 plugins {
     kotlin("jvm") version "1.6.10"
     id("io.gitlab.arturbosch.detekt") version "1.19.0"
     `java-library`
     id("me.champeau.jmh") version "0.6.1"
 }
-
 group = "org.example"
+
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -13,6 +27,7 @@ repositories {
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     google()
 }
+
 
 dependencies {
     implementation(kotlin("stdlib"))
@@ -23,9 +38,13 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
     implementation("io.github.microutils:kotlin-logging-jvm:2.1.21")
     implementation("ch.qos.logback:logback-classic:1.2.11")
-    implementation("org.jetbrains.lets-plot:lets-plot-common:2.3.0")
+//    implementation("org.jetbrains.lets-plot:lets-plot-common:2.3.0")
+    implementation("org.jetbrains.lets-plot:lets-plot-jfx:2.3.0")
     implementation("org.jetbrains.lets-plot:lets-plot-kotlin-jvm:3.2.0")
     implementation("org.jetbrains.lets-plot:lets-plot-image-export:2.3.0")
+    implementation("org.openjfx:javafx-base:18:${getJavaFXPlatform()}")
+    implementation("org.openjfx:javafx-swing:18:${getJavaFXPlatform()}")
+    implementation("org.openjfx:javafx-graphics:18:${getJavaFXPlatform()}")
 }
 
 tasks.test {
--- src/main/kotlin/homeworks/homework4/main.kt
+++ src/main/kotlin/homeworks/homework4/main.kt
@@ -9,17 +9,164 @@ import homeworks.homework4.qsort.QSortThreadPool
 import homeworks.homework4.qsort.partitions.DutchFlagPartition
 import homeworks.homework4.qsort.partitions.HoarePartition
 import homeworks.homework4.qsort.partitions.LomutoPartition
+import javafx.application.Platform
+import jetbrains.datalore.base.registration.Disposable
+import jetbrains.datalore.plot.MonolithicCommon
+import jetbrains.datalore.vis.swing.jfx.DefaultPlotPanelJfx
 import jetbrains.letsPlot.export.ggsave
 import jetbrains.letsPlot.geom.geomLine
 import jetbrains.letsPlot.geom.geomPoint
+import jetbrains.letsPlot.geom.geomSmooth
 import jetbrains.letsPlot.ggplot
 import jetbrains.letsPlot.ggsize
+import jetbrains.letsPlot.intern.Plot
+import jetbrains.letsPlot.intern.toSpec
 import kotlinx.coroutines.asCoroutineDispatcher
+import java.awt.Dimension
+import java.awt.GridLayout
 import java.util.concurrent.ForkJoinPool
+import javax.swing.*
+import javax.swing.WindowConstants.EXIT_ON_CLOSE
 import kotlin.time.Duration
 import kotlin.time.DurationUnit
 
 fun main() {
+    val data = getDataFrame()
+    val plots = mapOf(
+        "Default" to ggplot(data) { y = "durations"; x = "elementsCount"; color = "names" } +
+                ggsize(2000, 1500) +
+                geomPoint() +
+                geomLine(),
+        "With smooth" to ggplot(data) { y = "durations"; x = "elementsCount"; color = "names" } +
+                ggsize(2000, 1500) +
+                geomPoint() +
+                geomLine() + geomSmooth(),
+    )
+
+    val selectedPlotKey = plots.keys.first()
+    val controller = Controller(
+        plots,
+        selectedPlotKey,
+        false
+    )
+
+    val window = JFrame("Ерина мать трахал")
+    window.defaultCloseOperation = EXIT_ON_CLOSE
+    window.contentPane.layout = BoxLayout(window.contentPane, BoxLayout.Y_AXIS)
+
+    // Add controls
+    val controlsPanel = Box.createHorizontalBox().apply {
+        // Plot selector
+        val plotButtonGroup = ButtonGroup()
+        for (key in plots.keys) {
+            plotButtonGroup.add(
+                JRadioButton(key, key == selectedPlotKey).apply {
+                    addActionListener {
+                        controller.plotKey = this.text
+                    }
+                }
+            )
+        }
+
+        this.add(Box.createHorizontalBox().apply {
+            border = BorderFactory.createTitledBorder("Plot")
+            for (elem in plotButtonGroup.elements) {
+                add(elem)
+            }
+        })
+
+        // Preserve aspect ratio selector
+        val aspectRadioButtonGroup = ButtonGroup()
+        aspectRadioButtonGroup.add(JRadioButton("Original", false).apply {
+            addActionListener {
+                controller.preserveAspectRadio = true
+            }
+        })
+        aspectRadioButtonGroup.add(JRadioButton("Fit container", true).apply {
+            addActionListener {
+                controller.preserveAspectRadio = false
+            }
+        })
+
+        this.add(Box.createHorizontalBox().apply {
+            border = BorderFactory.createTitledBorder("Aspect ratio")
+            for (elem in aspectRadioButtonGroup.elements) {
+                add(elem)
+            }
+        })
+    }
+    window.contentPane.add(controlsPanel)
+
+    // Add plot panel
+    val plotContainerPanel = JPanel(GridLayout())
+    window.contentPane.add(plotContainerPanel)
+    controller.plotContainerPanel = plotContainerPanel
+    controller.rebuildPlotComponent()
+
+    SwingUtilities.invokeLater {
+        window.pack()
+        window.size = Dimension(850, 400)
+        window.setLocationRelativeTo(null)
+        window.isVisible = true
+    }
+}
+
+private class Controller(
+    private val plots: Map<String, Plot>,
+    initialPlotKey: String,
+    initialPreserveAspectRadio: Boolean
+) {
+    var plotContainerPanel: JPanel? = null
+
+    var plotKey: String = initialPlotKey
+        set(value) {
+            field = value
+            rebuildPlotComponent()
+        }
+    var preserveAspectRadio: Boolean = initialPreserveAspectRadio
+        set(value) {
+            field = value
+            rebuildPlotComponent()
+        }
+
+    fun rebuildPlotComponent() {
+        plotContainerPanel?.let {
+            val container = plotContainerPanel!!
+            // cleanup
+            for (component in container.components) {
+                if (component is Disposable) {
+                    component.dispose()
+                }
+            }
+            container.removeAll()
+
+            // build
+            container.add(createPlotPanel())
+            container.parent?.revalidate()
+        }
+    }
+
+    fun createPlotPanel(): JPanel {
+        // Make sure JavaFX event thread won't get killed after JFXPanel is destroyed.
+        Platform.setImplicitExit(false)
+
+        val rawSpec = plots[plotKey]!!.toSpec()
+        val processedSpec = MonolithicCommon.processRawSpecs(rawSpec, frontendOnly = false)
+
+        return DefaultPlotPanelJfx(
+            processedSpec = processedSpec,
+            preserveAspectRatio = preserveAspectRadio,
+            preferredSizeFromPlot = false,
+            repaintDelay = 10,
+        ) { messages ->
+            for (message in messages) {
+                println("[Example App] $message")
+            }
+        }
+    }
+}
+
+fun main1() {
     val data = getDataFrame()
     println(data)
 
@@ -51,7 +198,10 @@ fun getDataFrame(): MutableMap<String, Any> {
         val qSorts = mapOf<String, QSort<Int>>(
             "coroutines with Lomuto" to QSortCoroutines(LomutoPartition(), ForkJoinPool().asCoroutineDispatcher()),
             "coroutines with Hoare" to QSortCoroutines(HoarePartition(), ForkJoinPool().asCoroutineDispatcher()),
-            "coroutines with DutchFlag" to QSortCoroutines(DutchFlagPartition(), ForkJoinPool().asCoroutineDispatcher()),
+            "coroutines with DutchFlag" to QSortCoroutines(
+                DutchFlagPartition(),
+                ForkJoinPool().asCoroutineDispatcher()
+            ),
             "thread pool with Lomuto" to QSortThreadPool(LomutoPartition(), ForkJoinPool()),
             "thread pool with Hoare" to QSortThreadPool(HoarePartition(), ForkJoinPool()),
             "thread pool with DutchFlag" to QSortThreadPool(DutchFlagPartition(), ForkJoinPool()),
